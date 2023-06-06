4c3a75b3b2d661b596170d82e8b20cd2f9b04c9d
sangwoo <bakch9@naver.com>
2023-06-07 00:17:22 +0900

    commit_0606

--- src/app-config.js
+++ src/app-config.js
@@ -5,7 +5,7 @@ const hostname = window && window.location && window.location.hostname;
 if(hostname === 'localhost') {
     backendHost = "http://localhost:8080";
 } else {
-    backendHost = "http://todo-back.ap-northeast-2.elasticbeanstalk.com";
+    backendHost = "https://api.todokorea.net";
 }
 
 export const API_BASE_URL = backendHost;
